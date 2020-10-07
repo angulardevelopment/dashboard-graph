7ea32e6d22b8e0ffb403388bc2e43fc95e93ab70
Sunny Gupta <sunny.gupta@primussoft.com>
2020-10-07 11:02:27 +0530

    route guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { PostsComponent } from './modules/posts/posts.component';
+import { NewGuardGuard } from './new-guard.guard';
 
 const routes: Routes = [{
   path: '',
   component: DefaultComponent,
   children: [{
     path: '',
-    component: DashboardComponent
+    component: DashboardComponent,
+
+
   }, {
     path: 'posts',
-    component: PostsComponent
+    component: PostsComponent,
+    data: {role: 'Admin'},
+    canActivate: [NewGuardGuard],
   }]
 }];
 
